7ac1f4dba08e0e8c75e699aa8a3db2638263692a
yossef ashraf <yossefff2001@gmail.com>
2025-04-04 04:00:25 +0200

    Remove CommunicationServiceProvider and related configuration and service files

--- Contracts/CommunicationInterface.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Communication\Contracts;
-
-interface CommunicationInterface
-{
-    public function send(array $params): array;
-    public function validate(array $params): bool;
-}
